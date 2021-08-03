87ed5538435ece4d83963a60faddb37cb4ae3b42
Rachel Land <rachelland@Rachels-MacBook-Pro-2.local>
2021-08-03 15:59:09 -0400

    adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,16 @@ lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = 
 
 # determining p value and r squared value 
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = MechaCar_df))
+
+## Deliverable 2
+# read in csv
+suspensionCoil_df <- read.csv(file = "Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+head(suspensionCoil_df)
+
+# create total_summary dataframe using summarize function
+total_summary <- suspensionCoil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Stddev = sd(PSI))
+head(total_summary)
+
+# create lot_summary dataframe by using group_by() and summarize() functions. 
+lot_summary <- suspensionCoil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Stddev = sd(PSI))
+head(lot_summary)
